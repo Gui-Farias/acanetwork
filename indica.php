78f7fcce8dd7b83b6d4e7ff0d6f981049e665b7f
gui-farias <gui.farias@outlook.com.br>
2020-11-25 17:27:13 -0300

    subindo

---
+++ indica.php
@@ -0,0 +1,151 @@
+<div class="clearfix"></div>
+  <div class="section_holder7">
+    <div class="container">
+      <div class="section_title">
+        <h3 class="small">our clints</h3>
+        <br/>
+        <br/>
+        <h1 class="big">Indic<span>a</span></h1>
+      </div>
+      <div class="section_subtext three">
+        <p>Os clientes que compraram com nós, nos recomenda e sempre voltam a comprar conosco.</p>
+      </div>
+      <div class="clearfix"></div>
+      <div class="section_title_line"></div>
+      <div class="clearfix"></div>
+      <div class="one_full">
+        <section class="slider">
+          <div class="flexslider style2">
+            <ul class="slides">
+              <li>
+                <div class="clint_holder">
+                  <div class="clint">
+                    <div class="img_holder active">
+                      <div class="img"></div>
+                    </div>
+                  </div>
+                  <div class="text">
+                    <h5 class="lessmar2">Michael Nicholas</h5>
+                    <span>Developer - <span class="orange">Mediatricks</span></span>
+                    <p class="padd_top1">Lorem ipsum dolor sit amet, consectetuer adipiscing elit et justo sit Suspendisse et justo. Praesent mattis. </p>
+                  </div>
+                </div>
+                <!--end clint-->
+                
+                <div class="clint_holder two">
+                  <div class="clint two">
+                    <div class="img_holder two">
+                      <div class="img two"></div>
+                    </div>
+                  </div>
+                  <div class="text two">
+                    <h5 class="lessmar2">Charlotte Oliver</h5>
+                    <span>Support - <span class="orange">Mediatricks</span></span>
+                    <p class="padd_top1">Lorem ipsum dolor sit amet, consectetuer adipiscing elit et justo sit Suspendisse et justo. Praesent mattis. </p>
+                  </div>
+                </div>
+                <!--end clint--> 
+              </li>
+              <!--end item-->
+              
+              <li>
+                <div class="clint_holder">
+                  <div class="clint">
+                    <div class="img_holder active">
+                      <div class="img three"></div>
+                    </div>
+                  </div>
+                  <div class="text">
+                    <h5 class="lessmar2">Michael Nicholas</h5>
+                    <span>Marketing - <span class="orange">Mediatricks</span></span>
+                    <p class="padd_top1">Lorem ipsum dolor sit amet, consectetuer adipiscing elit et justo sit Suspendisse et justo. Praesent mattis. </p>
+                  </div>
+                </div>
+                <!--end clint-->
+                
+                <div class="clint_holder two">
+                  <div class="clint two">
+                    <div class="img_holder two">
+                      <div class="img four"></div>
+                    </div>
+                  </div>
+                  <div class="text two">
+                    <h5 class="lessmar2">Madison Ryan</h5>
+                    <span>Designer - <span class="orange">Mediatricks</span></span>
+                    <p class="padd_top1">Lorem ipsum dolor sit amet, consectetuer adipiscing elit et justo sit Suspendisse et justo. Praesent mattis. </p>
+                  </div>
+                </div>
+                <!--end clint--> 
+              </li>
+              <!--end item-->
+            </ul>
+          </div>
+        </section>
+      </div>
+    </div>
+  </div>
+
+  <!--end section 8-->
+  <div class="clearfix"></div>
+  <div class="section_holder9">
+    <div class="clints_section_topshape"></div>
+    <div class="container">
+      <h3 class="white lessmar">Nossos Clientes</h3>
+      <div class="section_title_line four"></div>
+      <div class="clearfix"></div>
+      <section class="slider">
+        <div class="flexslider style4 carousel">
+          <ul class="slides">
+            <li>
+              <div class="clint_logo">
+                <div class="hover_line"></div>
+                <img src="images/site-img13.png" alt=""/> </div>
+            </li>
+            <li>
+              <div class="clint_logo">
+                <div class="hover_line"></div>
+                  <img src="images/site-img14.png" alt=""/> </div>
+
+            </li>
+            <li>
+              <div class="clint_logo">
+                <div class="hover_line"></div>
+                  <img src="images/site-img15.png" alt=""/> </div>
+
+            </li>
+            <li>
+              <div class="clint_logo">
+                <div class="hover_line"></div>
+                  <img src="images/site-img16.png" alt=""/> </div>
+
+            </li>
+            <li>
+              <div class="clint_logo">
+                <div class="hover_line"></div>
+                  <img src="images/site-img17.png" alt=""/> </div>
+
+            </li>
+            <li>
+              <div class="clint_logo">
+                <div class="hover_line"></div>
+                  <img src="images/site-img18.png" alt=""/> </div>
+
+            </li>
+            <li>
+              <div class="clint_logo">
+                <div class="hover_line"></div>
+                  <img src="images/site-img13.png" alt=""/> </div>
+
+            </li>
+            <li>
+              <div class="clint_logo">
+                <div class="hover_line"></div>
+                  <img src="images/site-img13.png" alt=""/> </div>
+
+            </li>
+            
+          </ul>
+        </div>
+      </section>
+    </div>
+  </div>
